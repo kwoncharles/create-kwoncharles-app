e6e18549c52b26d9fb80eefda0e2040db42482dd
kwoncharles <hello.kwon.charles@gmail.com>
2021-04-17 10:53:40 +0900

    fix: wrong prompts default value

--- src/helper/prompts-utils.ts
+++ src/helper/prompts-utils.ts
@@ -4,12 +4,12 @@ export const confirmTailwindPropmpts: PromptObject<'confirmUseTailwind'> = {
   type: 'confirm',
   name: 'confirmUseTailwind',
   message: 'tailwindcss 옵션을 추가하지 않았습니다. 추가해드릴까요?',
-  initial: 'N',
+  initial: false,
 };
 
 export const confirmEslintPropmpts: PromptObject<'confirmUseEslint'> = {
   type: 'confirm',
   name: 'confirmUseEslint',
   message: 'eslint 옵션을 추가하지 않았습니다. 추가해드릴까요?',
-  initial: 'N',
+  initial: false,
 };
